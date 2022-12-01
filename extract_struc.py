75b20656217acaa68347836e9fa7d506cc9e7ae7
Tomoki Yamashita <Tomoki-YAMASHITA@users.noreply.github.com>
2022-12-01 19:26:20 +0900

    top option in extract_struc.py

--- extract_struc.py
+++ extract_struc.py
@@ -2,6 +2,9 @@
 #
 # extract_struc.py
 #
+#   2022/12/01 T. Yamashita
+#   --top option
+#
 #   2022/05/15 T. Yamashita
 #   --all option
 #
@@ -10,6 +13,7 @@
 #   pymatgen is required
 #
 import argparse
+import os
 import pickle
 
 
@@ -21,13 +25,18 @@ if __name__ == '__main__':
     example:
     - write cifs of ID 7 10 12 (output 7.cif, 10.cif, 12.cif)
       python3 extract_struc.py init_struc_data.pkl -i 7 10 12
-
     - write cifs of ID 7 10 12 (output 7.cif, 10.cif, 12.cif) with symmetry information
       python3 extract_struc.py init_struc_data.pkl -i 7 10 12 -s
 
+    - write cif of top k structures
+      python3 extract_struc.py ./data/pkl_data/opt_struc_data.pkl -t 3
+    - write cif of top k structures with the rank in the filename
+      python3 extract_struc.py ./data/pkl_data/opt_struc_data.pkl -t 3 -r
+    - write cif of top k structures with the rank in the filename and symmetory information
+      python3 extract_struc.py ./data/pkl_data/opt_struc_data.pkl -t 3 -rs
+
     - write all (output 0.cif, 1.cif, 2.cif ....)
       python3 extract_struc.py init_struc_data.pkl -a
-
     - write all (output 0.cif, 1.cif, 2.cif ....) with symmetry information
       python3 extract_struc.py init_struc_data.pkl -as
     '''
@@ -35,6 +44,8 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-a', '--all_id', help='flag for all structures', action='store_true')
     parser.add_argument('-i', '--index', help='structure ID', type=int, nargs='*')
+    parser.add_argument('-t', '--top', help='top k structures', type=int, nargs=1)
+    parser.add_argument('-r', '--rank', help='flag for rank in file names', action='store_true')
     parser.add_argument('-s', '--symmetrized', help='flag for symmetrized structure', action='store_true')
     parser.add_argument('infile', help='input file')
     args = parser.parse_args()
@@ -43,13 +54,32 @@ if __name__ == '__main__':
     with open(args.infile, 'rb') as f:
         struc_data = pickle.load(f)
 
-    # ---------- write cif
+    # ---------- index
     if args.index:   # not vacant
         for cid in args.index:
             if args.symmetrized:
                 struc_data[cid].to(fmt='cif', filename='{}.cif'.format(cid), symprec=0.1)
             else:
                 struc_data[cid].to(fmt='cif', filename='{}.cif'.format(cid))
+        raise SystemExit()
+
+    # ---------- top k
+    if args.top:   # not vacant
+        # ------ load rslt_data.pkl. It must be in the same directory as the input
+        rslt_path = os.path.dirname(args.infile) + '/rslt_data.pkl'
+        with open(rslt_path, 'rb') as f:
+            rslt_data = pickle.load(f)
+        # ------ top k data
+        top_k = rslt_data.sort_values(by=['E_eV_atom'], ascending=True).head(args.top[0])
+        for k, cid in enumerate(top_k.index):
+            if args.rank:
+                cifname = '{}_{}.cif'.format(k+1, cid)
+            else:
+                cifname='{}.cif'.format(cid)
+            if args.symmetrized:
+                struc_data[cid].to(fmt='cif', filename=cifname, symprec=0.1)
+            else:
+                struc_data[cid].to(fmt='cif', filename=cifname.format(cid))
         raise SystemExit()
 
     # ---------- all
@@ -59,3 +89,4 @@ if __name__ == '__main__':
                 struc.to(fmt='cif', filename='{}.cif'.format(cid), symprec=0.1)
             else:
                 struc.to(fmt='cif', filename='{}.cif'.format(cid))
+
